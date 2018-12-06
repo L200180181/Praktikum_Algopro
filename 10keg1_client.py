b711b95f5218ef425ebc7e6a72e9d74ac5c22808
L200180181 <44120264+L200180181@users.noreply.github.com>
2018-12-06 20:29:13 +0700

    Add files via upload

---
+++ 10keg1_client.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("",50004))
+s.listen(5)
+print ("Server komunikasi tentang data sudah siap")
+data = ''
+kamus = {'nama':'Muhammad Azka Nur Lutfi','NIM':'L200180181','angkatan':'2018'}
+
+while data.lower() !='keluar':
+    komm, addr = s.accept()
+    while data.lower() !='keluar':
+        data = komm.recv(1024)
+        if data.lower() == 'keluar':
+            s.close()
+            break
+        print ('Perintah:',data)
+        if kamus.has_key(data):
+            komm.send(kamus[data])
+        else:
+            komm.send('Maaf, perintah tidak dimengerti')
